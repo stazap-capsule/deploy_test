b73021dc01216cf8d2163976792ec3b9738f9504
Stanley Zapata <stanley@okcapsule.com>
2021-02-16 19:16:57 -0400

    Heroku build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
 const { parsed: localEnv } = require("dotenv").config();
 const withCSS = require("@zeit/next-css");
-
+const DotEnv = require("dotenv-webpack");
 const webpack = require("webpack");
-const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
+const path = require("path");
 
 module.exports = withCSS({
-  webpack: config => {
-    const env = { API_KEY: apiKey };
-    config.plugins.push(new webpack.DefinePlugin(env));
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+    config.node = {
+      fs: "empty",
+    };
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        API_KEY: JSON.stringify(process.env.SHOPIFY_API_KEY),
+      })
+    );
+    config.plugins.push(
+      new DotEnv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true,
+      })
+    );
+
     return config;
-  }
+  },
 });
